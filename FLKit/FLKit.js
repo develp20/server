115dcf659484c67cdfc9be37f2b97fe8f39079a8
William <w.gibbo3@icloud.com>
2018-10-06 00:45:52 +0100

    final fixes (4/?)

--- FLKit/FLKit.js
+++ FLKit/FLKit.js
@@ -2523,8 +2523,8 @@ module.exports = function(io, s3) {
 
                                                             // s3 migration timestamp
                                                             if(cDoc.info.postedAt >= 1538706511013) {
-                                                                streamURL = "https://" + bucketName + ".s3.amazonaws.com/public/" + cDoc.info.postID + ".mov";
-                                                                thumbURL = "https://" + bucketName + ".s3.amazonaws.com/public/" + cDoc.info.postID + ".png";
+                                                                streamURL = "https://" + bucketName + ".s3.us-east-1.amazonaws.com/public/videos/" + cDoc.info.postID + ".mov";
+                                                                thumbURL = "https://" + bucketName + ".s3.us-east-1.amazonaws.com/public/thumbnails/" + cDoc.info.postID + ".png";
                                                             }
 
 
